25ed5691a2510b3bd62bcbb645598c74366cf198
John Wei <johnzw89@gmail.com>
2014-02-08 20:51:45 -0500

    init file

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,3 +1,3 @@
-CarterRails::Application.config.account_sid = "AC22d8ffad69bcce8c543e1dde5a46aa4f"
-CarterRails::Application.config.auth_token = "807b52cf01c1871a9977a7cad4c39b49"
-CarterRails::Application.config.phone_number = "19172424688"
+NewPal::Application.config.account_sid = "AC22d8ffad69bcce8c543e1dde5a46aa4f"
+NewPal::Application.config.auth_token = "807b52cf01c1871a9977a7cad4c39b49"
+NewPal::Application.config.phone_number = "19172424688"
